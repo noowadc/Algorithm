fbbe57df3fa8a6e41caefe5f461aaef91c83c903
noowadc <139312401+noowadc@users.noreply.github.com>
2023-09-05 23:46:34 +0900

    [unrated] Title: 접미사 배열, Time: 0.18 ms, Memory: 77.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String my_string) {
+        
+        List<String> list = new ArrayList<>();
+        for(int i = 0;i<my_string.length();i++){
+            String str = my_string.substring(i, my_string.length());
+            list.add(str);
+        }
+        
+        String[] answer = new String[list.size()];
+        for(int i = 0;i<list.size();i++){
+            answer[i] = list.get(i);
+        }
+        Arrays.sort(answer);
+        
+        return answer;
+    }
+}
