3ff575bdafc250d70d8e44f5e6f0dad5c1e6c460
noowadc <139312401+noowadc@users.noreply.github.com>
2023-09-22 17:11:52 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.03 ms, Memory: 73.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int[] emergency) {
+        int[] answer = new int[emergency.length];
+        for(int i = 0;i<emergency.length;i++) {
+            answer[i] = 1;
+            for(int j = 0;j<emergency.length;j++){
+                if(i != j && emergency[i]<emergency[j]){
+                    answer[i]++;
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
