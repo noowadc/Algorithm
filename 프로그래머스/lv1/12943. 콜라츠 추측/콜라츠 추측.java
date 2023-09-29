78cbbad78e00930c967265437a01f8381aaaa0a9
noowadc <139312401+noowadc@users.noreply.github.com>
2023-09-29 20:56:16 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int solution(long num) {
+        int answer = 0;
+        int cnt = 0;
+        if(num == 1) cnt = 0;
+        while(num != 1 && num!=1){
+            if(num %2 == 0) {
+                num = num/2;
+                cnt++;
+            }
+            else {
+                num = num*3+1;
+                cnt++;
+            }
+            if(cnt>500) {
+                cnt = -1;
+                break;
+            }
+        }
+        answer = cnt;
+        return answer;
+    }
+}
