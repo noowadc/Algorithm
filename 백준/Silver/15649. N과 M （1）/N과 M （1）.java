8a80b0df18f8ca7ddfc04d4b20b943640e54178e
noowadc <139312401+noowadc@users.noreply.github.com>
2023-10-18 00:05:57 +0900

    [Silver III] Title: N과 M (1), Time: 2476 ms, Memory: 70764 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,47 +1,43 @@
-
 import java.util.Scanner;
 
 public class Main {
-	
-	static int N,M;
-	static int[] nums;
+    static int N, M;
 	static int[] arr;
 	static boolean[] visited;
 	
-	static void permutation(int idx) {
-		
-		if(idx == M) {
-			for(int i : arr) {
-				System.out.print(i + " ");
-			}
-			System.out.println();
-			
-			return;
-		}
+public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	N = sc.nextInt();//자연수
+	M = sc.nextInt();//뽑는 개수
+	arr = new int[M+1];
+	visited = new boolean[N+1];
+	
+	
+		DFS(0);
+	
+}
 
-		for(int i = 0; i < N; i++) {
-			if(visited[i]) continue;
-			visited[i] = true;
-			arr[idx] = nums[i];
-			permutation(idx+1);
-			visited[i] = false;
+public static void DFS(int depth) {
+	if(depth == M) {//뽑아야 하는 개수만큼 뽑으면 출력
+		for(int i = 0;i<M;i++) {
+			System.out.print(arr[i] +" ");
+		}
+		System.out.println();
+		return;
+	}
+	
+	for(int i = 1;i<=N;i++) {
+		if(!visited[i]) {//안뽑았으면
+			visited[i] = true;//뽑은 거 체크
+			arr[depth] = i; //뽑고
+			DFS(depth+1);//다음 탐색
+			visited[i] = false;//체크 풀기
+			
 		}
 		
 	}
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		nums = new int[N];
-		arr = new int[M];
-		visited = new boolean[N];
-		
-		for(int i = 0; i < N; i++) {
-			nums[i] = i+1;
-		}
-		permutation(0);
-	}
-	
+}
+    
+    
 }
