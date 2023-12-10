5acf3b42575e701bbb09f7b46d9622bcf5f3309f
noowadc <139312401+noowadc@users.noreply.github.com>
2023-12-10 17:57:43 +0900

    [level unrated] Title: 소인수분해, Time: 0.04 ms, Memory: 75.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int n) {
+        List<Integer> list = new ArrayList<>();
+        
+        //자연수 n의 소인수를 구하기 위한 반복문
+        for(int i = 2; i <= n; i++) {
+            /*나누어 떨어지면 i는 소인수, 
+            몫이 2이상인 경우도 있기 때문에 안 나눠질 때까지 나눠줌*/
+            if(n % i == 0) {
+                while(n % i == 0) {
+                    n /= i;
+                }
+                list.add(i); //구한 소인수를 list에 넣어줌
+            }
+        }
+        int[] answer = new int[list.size()]; //소인수 갯수와 같은 길이로 설정
+        //list의 값들을 answer 배열에 넣어줌
+        for(int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        return answer; //소인수 배열 return
+    }
+}
