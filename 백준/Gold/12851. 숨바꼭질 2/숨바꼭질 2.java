82bd5bf5f69d8a70e5ceeeaf048f970f35c76499
noowadc <139312401+noowadc@users.noreply.github.com>
2023-12-11 00:11:49 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 184 ms, Memory: 24752 KB -BaekjoonHub

---
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	
+	static int[] dx = {-1, 1, 2};
+	static int cnt=0, res = Integer.MAX_VALUE;
+
+	public static void main(String[] args) throws IOException{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		
+		bfs(n, k);
+		System.out.println(res);
+		System.out.println(cnt);
+	}
+	
+	static void bfs(int start, int destination) {
+		Queue<Integer> q = new LinkedList<>();
+		int[] move = new int[100_001];
+		q.add(start);
+		
+		while(!q.isEmpty()) {
+			int pos = q.poll();
+			
+			if(res < move[pos]) return;
+			if(move[pos]<=res && pos == destination) {
+				res = move[pos];
+				cnt++;
+			}
+
+			for(int i=0; i<3; i++) {
+				int next = pos;
+				if(i==2) {
+					next = pos*dx[i];
+				}else {
+					next = pos + dx[i];	
+				}
+				
+				if (next>=0 && next <100001) {
+					if(move[next] ==0 || move[next] >= move[pos] + 1) {
+						move[next] = move[pos]+1;
+						q.add(next);
+					}
+				}
+			}
+		}
+	}
+}
