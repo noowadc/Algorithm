f62c54272b7fdc9ef2a6b2ed631a028a70d9dcf9
noowadc <139312401+noowadc@users.noreply.github.com>
2024-01-01 20:41:34 +0900

    [level unrated] Title: 공 던지기, Time: 0.04 ms, Memory: 79.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120843. 공 던지기/공 던지기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int[] numbers, int k) {
+        int answer = 0;
+        
+        
+        answer = numbers[(k*2-2)%numbers.length];
+        
+        
+        return answer;
+    }
+}
